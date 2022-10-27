7fda37b401adcaebe5e81fc231756c07d8673561
Simion <simion.okola@student.moringaschool.com>
2022-10-28 00:37:22 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 30, "F", "brown", "calm", 0),
+(2,"Rowdy", 20, "M", "black", "intense", 1),
+(3,"Tabitha", 16, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 9, "M", "Green", "Slimy", 0),
+(5,"Melissa", 23, "M", "grey", "goofy", 1),
+(6,"Grinch", 8, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 16, "F", "Blue", "naive", 1),
+(8,null, 10, "F", "black", "cool", 1);
